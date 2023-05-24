9133599cd997d538bd1c84cc984e2fb3ea2eac66
nakasone kouta <n23022@std.it-college.ac.jp>
2023-05-24 08:52:11 +0000

    chap4 first

---
+++ chap4/jk8lugl.py
@@ -0,0 +1,14 @@
+n = input("type a number:")
+n = int(n)
+result = "{n} is even" if n % 2 == 0 else f"{n} is odd"
+print(result)
+
+n = input("type a number:")
+n = int(n)
+result = "{n} is even" if n % 2 == 0 else f"{n} is odd"
+print(result)
+
+n = input("type a number:")
+n = int(n)
+result = "{n} is even" if n % 2 == 0 else f"{n} is odd"
+print(result)
